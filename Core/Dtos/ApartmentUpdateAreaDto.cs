29c31e0c2ca434766629a0744337e0a08cddfae9
Vitályos Norbert <vitalyosn@gmail.com>
2023-03-29 20:53:07 +0300

    added ApartmentUpdateAreaDto

---
+++ Core/Dtos/ApartmentUpdateAreaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class ApartmentUpdateAreaDto
+    {
+        public int ApartmentId { get; set; }
+
+        public float TotalArea { get; set; }
+
+        public float UsableArea { get; set; }
+    }
+}
