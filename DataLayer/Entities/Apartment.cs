50306f356da4ce3e332eb4d3702e965fb4e3cff1
Vitályos Norbert <vitalyosn@gmail.com>
2023-03-29 19:59:02 +0300

    added Apartment and ApartmentAddress entities

---
+++ DataLayer/Entities/Apartment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class Apartment
+    {
+        public int Id { get; set; }
+
+        public int AddressId { get; set; }  
+
+        public ApartmentAddress Address { get; set; }
+
+        public float TotalArea { get; set; }
+
+        public float UsableArea { get; set; }
+
+        public int NumberOfRooms { get; set; }
+
+        // String for now so as not to complicate this
+        // simple homework pointlessly
+        public string Owner { get; set; }
+    }
+}
---
+++ DataLayer/Entities/ApartmentAddress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    /// <summary>
+    /// Entity representing the address of an apartment;
+    /// as such, it also contains information about its
+    /// location within the apartment building it resides in.
+    /// All properties are strings to account for unusual
+    /// cases of non-numeric address parts.
+    /// </summary>
+    public class ApartmentAddress
+    {
+        public required int Id { get; set; }
+
+        public required int ApartmentId { get; set; }
+
+        public required Apartment Apartment { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Street { get; set; }
+
+        public required string Number { get; set; }
+
+        public required string BuildingNumber { get; set; }
+
+        public required string Section { get; set; }
+
+        public required string Floor { get; set; }
+
+        public required string ApartmentNumber { get; set; }
+    }
+}
