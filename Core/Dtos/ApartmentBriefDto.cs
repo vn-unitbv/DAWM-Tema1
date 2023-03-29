426092089b5c552927a8f3ea1fb3ca52807814c1
Vitályos Norbert <vitalyosn@gmail.com>
2023-03-29 20:44:48 +0300

    added ApartmentDto and ApartmentBriefDto

---
+++ Core/Dtos/ApartmentBriefDto.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class ApartmentBriefDto
+    {
+        public int Id { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+    }
+}
---
+++ Core/Dtos/ApartmentDto.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class ApartmentDto
+    {
+        public int Id { get; set; }
+
+        public ApartmentAddress Address { get; set; }
+
+        public float TotalArea { get; set; }
+
+        public float UsableArea { get; set; }
+
+        public int NumberOfRooms { get; set; }
+
+        // String for now so as not to complicate this
+        // simple homework pointlessly
+        public string Owner { get; set; }
+    }
+}
